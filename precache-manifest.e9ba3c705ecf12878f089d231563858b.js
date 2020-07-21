bf2c4f2579904bc90446f25b8f6caf0b4cc897d3
Tito mbugua <iamtitombugua@gmail.com>
2020-07-21 17:49:10 +0400

    Updates

---
+++ precache-manifest.e9ba3c705ecf12878f089d231563858b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8445613c57c5e2eeaf4cc7360edf434",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "67af3a618dc0eb1072c3",
+    "url": "/my-portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "4fc0dd1053f13097581d",
+    "url": "/my-portfolio/static/css/main.264826f1.chunk.css"
+  },
+  {
+    "revision": "67af3a618dc0eb1072c3",
+    "url": "/my-portfolio/static/js/2.fe69c9dc.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-portfolio/static/js/2.fe69c9dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc0dd1053f13097581d",
+    "url": "/my-portfolio/static/js/main.080fbdbe.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
